49d6d17df99c0bdcb9f860a86e4ada12710743bf
Kelly Williams <Kewilliams591@gmail.com>
2018-09-12 15:16:03 -0400

    another

--- foodAPI.js
+++ foodAPI.js
@@ -8,49 +8,89 @@ const searchTerm = document.querySelector('.search');
 const searchForm = document.querySelector('form');
 const submitBtn = document.querySelector('.submit');
 //RESULTS NAVIGATION
-const nextBtn = document.querySelector('.next');
-const previousBtn = document.querySelector('.prev');
 const nav = document.querySelector('nav');
-
-// const recipe = document.getElementById('recipe').value;
-let pageNumber = 0;
-// console.log('PageNumber: ', pageNumber);
-let displayNav = false;
+// console.log('PageNumber: ', pageNumber)
 //RESULTS SECTION
-const section = document.querySelector('.results');
+const section = document.querySelector('section');
 searchForm.addEventListener('submit', fetchResults);
-// nextBtn.addEventListener('click', nextPage);
-// previousBtn.addEventListener('click', previousPage);
-nav.style.display = 'none';
+// nextBtn.addEventListener('click', json.from);
+// previousBtn.addEventListener('click', json.to);
+
+
+
 
 function fetchResults(e) {
     e.preventDefault();
     console.log(e);
-
     url = `${baseURL}?q=${searchTerm.value}&app_id=${appId}&app_key=${key}`;
-console.log(url);
-    fetch(url)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (json) {
+    console.log(url);
+     fetch(url)  // promise
+    .then(function(result) {
+        return result.json();
+    })
+    .then(function(json) {
             displayResults(json);
             console.log(json);
         })
-        .catch(function(e) {
-           console.log("wrong");
-        })
-      
-
-        
     }
-     function displayResults(json) {
-         cosole.log("Display Results", json);
+    
+    function displayResults(json){
+        while(section.firstChild){ // ask what this means
+        section.removeChild(section.firstChild);
+       }
+       let recipes = json.hits;
+console.log(recipes);
+
+
+
+if(recipes.length === 0){
+    console.log("No results");
+}  else {
+for(let i = 0; i < recipes.length; i++){
+    console.log(i);
+       let recipe= document.createElement('section');
+       let link = document.createElement('a');
+       let img = document.createElement('img');
+       let para= document.createElement('h2');
+       let clearfix = document.createElement('div');
+       let calCount = document.createElement('p')
+       let current = recipes;
+        link.href = current[i].recipe.url;
+        link.textContent = current[i].recipe.label;
+        para.textContent = current[i].recipe.healthLabels;
+        img.src= current[i].recipe.image;
+        calCount.textContent =`CALORIES:" ${current[i].recipe.calories}`;
+
+    
+// appending
+    clearfix.setAttribute('class', 'clearfix');
+    section.appendChild(recipe);
+    para.appendChild(link);
+       section.appendChild(link);
+       section.appendChild(para);
+       section.appendChild(calCount);
+       section.appendChild(img);
+       section.appendChild(clearfix);
+    }
+   
+}
+}
+
+
+window.onload = function () {
+    // Array of Images
+     var backgroundImg=["./assets/corndogBackdrop.jpg" ,
+     "./assets/burgerAndFries.jpg",
+     "./assets/fries.jpg",
+     "./assets/m&ms.jpg" ,
+     "./assets/twoBurgersTatts.jpg",
+                       ]
+
+       setInterval(changeImage, 2000);
+      function changeImage() {   
+       var i = Math.floor((Math.random() * 6));
+
+       document.body.style.backgroundImage = "url('"+backgroundImg[i]+"')";
+
      }
-//  recipe = json.result.docs;
-//  if (articles.length === 10){
-//     nav.style.display = 'block';
-// } else {
-//     nav.style.display= 'none';
-// }
-// console.log(json);
+   }
